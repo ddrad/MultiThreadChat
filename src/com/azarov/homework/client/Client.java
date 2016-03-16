543e4b2232e81960e58be30147a483b2033b69d3
ddrad <ddrad@mail.ru>
2016-03-16 23:19:37 +0300

    add function pass file

--- src/com/azarov/homework/client/Client.java
+++ src/com/azarov/homework/client/Client.java
@@ -2,10 +2,7 @@ package com.azarov.homework.client;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 import javax.swing.JFrame;
@@ -20,14 +17,44 @@ import javax.swing.JTextField;
 public class Client {
 
     public static void main(String[] args) throws IOException {
+
         String serverAddress = JOptionPane.showInputDialog(
                 "Enter IP Address of a machine that is running the date service on port 9090:");
 
+        String downloadDirectory = JOptionPane.showInputDialog(
+                "Enter directory where you want save file:");
+
+        File directory = new File(downloadDirectory);
+
+        if(!directory.exists() && !directory.isDirectory() ){
+            directory.mkdirs();
+        }
+
         Socket s = new Socket(serverAddress, 9090);
 
-        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String answer = input.readLine();
-        JOptionPane.showMessageDialog(null, answer);
+        FileOutputStream fileOutputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
+        int bytesRead;
+        int current = 0;
+        byte [] mybytearray  = new byte [16*1024];
+
+        bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(downloadDirectory + File.separator + "tmp.doc"));
+
+        InputStream inputStream = s.getInputStream();
+
+        bytesRead = inputStream.read(mybytearray,0,mybytearray.length);
+        current = bytesRead;
+
+        do {
+            bytesRead = inputStream.read(mybytearray, current, (mybytearray.length-current));
+            if(bytesRead >= 0) current += bytesRead;
+        } while(bytesRead > -1);
+
+        bufferedOutputStream.write(mybytearray, 0 , current);
+        bufferedOutputStream.flush();
+        System.out.println("File  downloaded (" + current + " bytes read)");
+
+        JOptionPane.showMessageDialog(null, "File  downloaded (" + current + " bytes read)");
         System.exit(0);
     }
 }
--- src/com/azarov/homework/server/Server.java
+++ src/com/azarov/homework/server/Server.java
@@ -1,11 +1,12 @@
 package com.azarov.homework.server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
 import java.util.HashSet;
 
@@ -46,16 +47,33 @@ public class Server {
         public void run() {
             System.out.println(socket.toString());
             PrintWriter out = null;
+            File file = new File("D:\\Workspace\\Projects\\TradeView\\Task_Java_2.doc");
+            FileInputStream fileInputStream = null;
+            BufferedInputStream bufferedInputStream = null;
+            OutputStream outputStream = null;
+
+            BufferedOutputStream bufferedOutputStream = null;
+
             try {
-                while (true) {
-                    out = new PrintWriter(socket.getOutputStream(), true);
-                    out.println(new Date().toString());
-                }
+                bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+                byte [] byteArray  = new byte [(int)file.length()];
+
+                bufferedInputStream.read(byteArray,0,byteArray.length); // copied file into byteArray
+
+                //sending file through socket
+                bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
+                System.out.println("Sending " + file.getName() + "( size: " + byteArray.length + " bytes)");
+                bufferedOutputStream.write(byteArray,0,byteArray.length);
+                bufferedOutputStream.flush();
+                System.out.println("Done.");
+
             } catch (IOException e) {
                 System.out.println(e.getMessage());
             }
             finally {
                 try {
+                    bufferedInputStream.close();
+                    bufferedOutputStream.close();
                     socket.close();
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
